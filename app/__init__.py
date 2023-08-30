226ef7563524210d013ff270e36fa42215232115
Eloka Chima <eloka.chima@gmail.com>
2023-08-30 19:49:04 +0100

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,13 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from app.blueprints.base_blueprint import BaseBlueprint
 from flask_migrate import Migrate
+from flask_cors import CORS
 from app.utils.helper import get_env
 
 app = Flask(__name__)
+
+CORS(app)
+
 app.config.from_object(get_env("FLASK_ENV"))
 
 bcrypt = Bcrypt(app)
